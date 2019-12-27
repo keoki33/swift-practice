f13dc26c6efb7d71d289653ab661b8639b358455
George <gwong323@yahoo.com>
2019-12-27 22:53:13 +0000

    lesson 1-3

---
+++ swift-practice.swift
@@ -0,0 +1,33 @@
+import UIKit
+var str = "Hello, Playground"
+var str1 = "test"
+
+print(str1)
+print(str+" "+str1)
+
+let consName = "constant"
+
+var firstName:String = "Tom"
+var number:Int = 5
+var boolean:Bool = true
+
+let a = 11
+
+if (a<10)
+{print("a is less than 10")}
+else
+{print("a is more than 10")}
+
+let b = "a"
+
+switch b {
+case "c", "d" :
+   print("letter is c or d")
+case "a":
+    print("letter is a")
+default:
+print("no match")}
+
+for <#item#> in <#items#> {
+    print("yo")
+}
