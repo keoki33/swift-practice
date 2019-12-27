c380a5b3c5730965b65aee84ca9bb606a00a6173
George <gwong323@yahoo.com>
2019-12-27 23:25:23 +0000

    lesson 6

--- swift-practice.swift
+++ swift-practice.swift
@@ -28,6 +28,45 @@ case "a":
 default:
 print("no match")}
 
-for <#item#> in <#items#> {
+
+var num = 0
+for x in 1...5 {
     print("yo")
+    num += x
+    print(num)
 }
+print(num)
+
+var c = 1
+
+while c < 10{
+    print("less than 10")
+       c += 1
+}
+
+repeat {
+       print("less than 20")
+        c += 1
+
+} while c < 20
+
+
+
+func testFunction() {
+    print("testFunction has ran")
+}
+
+testFunction()
+
+var num1 = 0
+
+
+func addNum() -> Int {
+    let a = 1
+    let b = 2
+    let c = a + b
+    return c
+}
+
+let sum = addNum()
+print(sum)
